3060be585278b5a6ed2a9a93c623e979a8422ef1
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-11-06 18:49:51 +0200

    updated

--- Cat-React-Router/src/main.jsx
+++ Cat-React-Router/src/main.jsx
@@ -28,3 +28,18 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
+/* existing imports */
+import Root from "./routes/root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
